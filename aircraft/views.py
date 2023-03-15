3fc1523da003b2899a1db19519a0421a3eb65771
dominiusd <piort.bogdan@gmail.com>
2023-03-15 02:35:19 +0200

    first

--- aircraft/views.py
+++ aircraft/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
 import json
 import requests
+
+from django.shortcuts import render
+from django.core.paginator import Paginator
 
 
 def search_aircraft(request):
@@ -9,8 +11,24 @@ def search_aircraft(request):
 
     url = 'https://dir.aviapages.com/api/aircraft/'
     headers = {'Authorization': 'Token BN442rPF4zTfWAGQDqrZgjRWKznDfxUg9VK'}
-    response = requests.get(url, headers=headers)
+    params = {'images': True, 'page':30}
+    response = requests.get(url, headers=headers, params=params)
+    print(response.url)
     print(type(response.text), response.status_code)
-    data = json.loads(response.text)
-    print(data['count'])
-    return render(request, 'aircraft/search_aircraft.html', {'data': data})
+    data = json.loads(response.text)['results']
+    dd = json.loads(response.text)['per_page']=299
+    print(len(data))
+
+    # paginator
+    paginator = Paginator(data, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+
+
+
+
+
+
+    # print(data)
+    return render(request, 'aircraft/search_aircraft.html', {'data': data, 'page_obj':page_obj})
