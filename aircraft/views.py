2d231d889f18d03f91f446f78dc53bfdc4b7ca32
Piort-Bogdan <piort.bogdan@gmail.com>
2023-03-20 03:03:45 +0200

    V.1

--- aircraft/views.py
+++ aircraft/views.py
@@ -1,6 +1,4 @@
 import json
-import os
-
 import requests
 import concurrent.futures
 
@@ -14,81 +12,56 @@ from aircraft_search.settings import API_TOKEN
 class AircraftList(TemplateView):
     ''' Aicraft API list View '''
 
-
     template_name = 'aircraft/search_aircraft.html'
-    HEADERS = {'Authorization': API_TOKEN}
+    # HEADERS = {'Authorization': API_TOKEN}
+    HEADERS = {'Authorization': 'Token BN442rPF4zTfWAGQDqrZgjRWKznDfxUg9VK'}
     URL = 'https://dir.aviapages.com/api/'
 
     def get(self, request, *args, **kwargs):
 
-        def get_aircraft_list(page):
-
-            ''' Get aircraft list '''
-
-            data = []
-            try:
-                url = self.URL + 'aircraft/'
-                if len(request.GET.get('serial')) < 2 or len(request.GET.get('tail')) < 2:
-                    query_serial, query_tail = ' ', ' '
-                query_serial = request.GET.get('serial', '')
-                query_tail = request.GET.get('tail', '')
-                params = {'images': True, 'page': page, 'search_tail_number': query_tail,
-                          'search_serial_number': query_serial, 'features': True}
-                response = requests.get(url, headers=self.HEADERS, params=params)
-                aircraft_list = json.loads(response.text)['results']
-                for item in aircraft_list:
-                    data.append(item)
-                print(f'Downloaded page aircraft {page}')
-            except Exception as e:
-                print(f'Error fetching page aircraft {page}: {e}')
-            return data
-
-        data = []
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            future = [executor.submit(get_aircraft_list, page) for page in range(1, 593)]  # 593
-
-            for f in concurrent.futures.as_completed(future):
-                result = f.result()
-                data += result
-
-        def get_company_detail(data):
-
-            ''' GET company detail information '''
-
-            companies_list = []
-            for company in data:
-                try:
-                    params_company = {'search_name': company['company_name']}
-                    url_company = self.URL + 'companies/'
-                    response = requests.get(url_company, headers=self.HEADERS, params=params_company)
-                    companies = json.loads(response.text)['results']
-                    for company_json in companies:
-                        companies_list.append(company_json)
-                except Exception as e:
-                    print(f'Company detail getting error - {e}')
-            return companies_list
-
-        def get_airport_detail(data):
-
-            ''' GET airport detail information '''
-
-            airport_list = []
-            for icao in data:
-                try:
-                    url_airports = self.URL + 'airports/'
-                    params_airport = {'search_icao': icao['home_base']}
-                    response = requests.get(url_airports, headers=self.HEADERS, params=params_airport)
-                    airports = json.loads(response.text)['results']
-                    for airport in airports:
-                        airport_list.append(airport)
-                except Exception as e:
-                    print(f'Airport detail gettin error - {e}')
-            return airport_list
-
+        data_list = []
+        companies_list = []
         airport_list = []
+        count = self.get_page_count(request)
 
-        #pagination
-        paginator = Paginator(data, 300)
+        # make item list form search
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            aircraft_future = [executor.submit(self.get_aircraft_list, request, page) for page in range(1, round(count))]
+
+            ok = concurrent.futures.wait(aircraft_future)
+            try:
+                for future in ok.done:
+                    result = future.result()
+                    data_list += result
+                print(len(data_list))
+            except Exception as e:
+                print(f'Error - {e}')
+
+        # make list of companies from search list
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            companies_future = [executor.submit(self.get_company_detail, data) for data in data_list]
+
+            try:
+                for future_comp in concurrent.futures.as_completed(companies_future):
+                    result_comp = future_comp.result()
+                    companies_list += result_comp
+                print(f'{len(companies_list)}, List Company')
+            except Exception as exp:
+                print(f'Getting error = {exp}')
+
+        # make list of airports from search list
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            airports_future = [executor.submit(self.get_airport_detail, data) for data in data_list]
+            try:
+                for future_airport in concurrent.futures.as_completed(airports_future):
+                    result_airport = future_airport.result()
+                    airport_list += result_airport
+                print(f'airport list {len(airport_list)}')
+            except Exception as exp:
+                print(f'Getting error = {exp}')
+
+        # pagination
+        paginator = Paginator(data_list, 300)
         page_number = request.GET.get('page')
         page_obj = paginator.get_page(page_number)
 
@@ -98,10 +71,65 @@ class AircraftList(TemplateView):
             current_page = 1
 
         context = {
-            'data': data,
             'page_obj': page_obj,
             'current_page': current_page,
-            'companies': get_company_detail(data),
-            'airports': get_airport_detail(data)
+            'companies': companies_list,
+            'airports': airport_list,
         }
         return render(request, self.template_name, context)
+
+    def get_aircraft_list(self, request, page):
+
+        ''' Get aircraft list '''
+
+        if request.GET.get('serial') == '' and request.GET.get('tail') == '':
+            params = {'images': True, 'features': True }
+        else:
+            url = self.URL + 'aircraft/'
+            query_serial = request.GET.get('serial')
+            query_tail = request.GET.get('tail')
+            params = {'features': True, 'images': True, 'ordering': 'serial_number', 'search_tail_number': query_tail,
+                      'search_serial_number': query_serial}
+            try:
+                response = requests.get(url, headers=self.HEADERS, params=params)
+                aircraft = json.loads(response.text)['results']
+            except Exception as e:
+                print(f'error - {e}')
+            return aircraft
+
+    def get_company_detail(self, data):
+        
+        ''' GET company detail information '''
+        companies = []
+        try:
+            params_company = {'search_name': data['company_name']}
+            url_company = self.URL + 'companies/'
+            response = requests.get(url_company, headers=self.HEADERS, params=params_company)
+            companies = json.loads(response.text)['results']
+        except Exception as e:
+            print(f'Get_company_detail Error - {e}')
+        return companies
+
+    def get_airport_detail(self, data):
+
+        ''' GET airport detail information '''
+        airports = []
+        try:
+            url_airports = self.URL + 'airports/'
+            params_airport = {'search_icao': data['home_base']}
+            response = requests.get(url_airports, headers=self.HEADERS, params=params_airport)
+            airports = json.loads(response.text)['results']
+        except Exception as e:
+            print(f'Airport detail gettin error - {e}')
+        return airports
+
+    def get_page_count(self, request):
+
+        url = self.URL + 'aircraft/'
+        query_serial = request.GET.get('serial')
+        query_tail = request.GET.get('tail')
+        params = {'features': True, 'images': True, 'ordering': 'serial_number', 'search_tail_number': query_tail,
+                  'search_serial_number': query_serial}
+        response = requests.get(url, headers=self.HEADERS, params=params)
+        count = json.loads(response.text)['count']/20
+        return count
